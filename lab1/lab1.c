3bfb01d0fce54b3a1f85060d717e7b780f0ed95a
Edgar Cherkasov <edgar.cherkasov@yandex.ru>
2014-10-28 01:32:31 +0300

    some changes

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 
 size_t ram_size();
+size_t proc_cnt();
 
 int main()
 {
 	printf("Mem size: %ld\n", ram_size());
+	printf("Proc: %ld\n", proc_cnt());
 
 	exit(EXIT_SUCCESS);
 }
@@ -17,4 +19,9 @@ size_t ram_size()
 	long page_size = sysconf(_SC_PAGE_SIZE);
 
 	return pages * page_size;
+}
+
+size_t proc_cnt()
+{
+	return sysconf(_SC_NPROCESSORS_ONLN);
 }
